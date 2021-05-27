8e8ea8455d5985889b8307f4a125caef19119844
Jacob Hall <jwhall@email.wm.edu>
2021-05-27 13:43:36 -0400

    adding neovim config

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ stow zsh
 stow vim
 stow zathura
 stow ssh
+
+# if neovim isn't installed on this computer...
+if command -v nvim &> /dev/null
+then
+	stow nvim
+else
+	# recommend that you install it
+	echo "I usually use nvim. I recommend you install it and then rerun this script."
+fi
