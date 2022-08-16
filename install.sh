af021422f7c181a1d1d2531b5f43479591437e95
Jacob Hall <email@jacobhall.net>
2022-08-16 10:21:21 -0400

    removing neovim and vim plugins

--- install.sh
+++ install.sh
@@ -5,12 +5,3 @@ stow zsh
 stow vim
 stow zathura
 stow ssh
-
-# if neovim isn't installed on this computer...
-if command -v nvim &> /dev/null
-then
-	stow nvim
-else
-	# recommend that you install it
-	echo "I usually use nvim. I recommend you install it and then rerun this script."
-fi
